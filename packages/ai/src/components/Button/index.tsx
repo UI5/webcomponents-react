043b0c0f9d4f694a15c88d49de93969d32476d28
Lukas Harbarth <lukas.harbarth@sap.com>
2025-09-17 08:40:25 +0200

    run ai wrapper script

--- packages/ai/src/components/Button/index.tsx
+++ packages/ai/src/components/Button/index.tsx
@@ -114,7 +114,7 @@ interface ButtonPropTypes
  *
  *
  *
- * __Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)
+ * __Note__: This is a UI5 Web Component! [Repository](https://github.com/UI5/webcomponents) | [Documentation](https://ui5.github.io/webcomponents/)
  *
  * @since [2.0.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v2.0.0) of __@ui5/webcomponents-ai__.
  * @experimental The Button and ButtonState web components are availabe since 2.0 under an experimental flag and their API and behaviour are subject to change.
--- packages/ai/src/components/PromptInput/index.tsx
+++ packages/ai/src/components/PromptInput/index.tsx
@@ -177,7 +177,7 @@ interface PromptInputPropTypes
  *
  *
  *
- * __Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)
+ * __Note__: This is a UI5 Web Component! [Repository](https://github.com/UI5/webcomponents) | [Documentation](https://ui5.github.io/webcomponents/)
  * @experimental The **@ui5/webcomponents-ai** package is under development and considered experimental - components' APIs are subject to change.
  */
 const PromptInput = withWebComponent<PromptInputPropTypes, PromptInputDomRef>(
