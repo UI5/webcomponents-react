a59231f568a6c8ab325d65fd75caa7b123dd3ec0
Lukas Harbarth <lukas.harbarth@sap.com>
2026-04-15 13:45:09 +0200

    rename vite-monorepo-exports-plugin to vite-monorepo-aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import IstanbulPlugin from 'vite-plugin-istanbul';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { generateMonorepoAliases } from './config/vite-monorepo-exports-plugin.js';
+import { generateMonorepoAliases } from './config/vite-monorepo-aliases.js';
 
 // https://vitejs.dev/config/
 
