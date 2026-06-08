0e5c6834e67581e7a4a002d79f56fc0edd86496a
Lukas742 <lukas.harbarth@sap.com>
2026-06-08 13:53:12 +0000

    Deploying to gh-pages from @ UI5/webcomponents-react@2631dfa0968a6b98dcf0074f328d6ac56213c48c 🚀

--- nightly/assets/formatter-EIJCOSYU-B46VKkH8.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{Ag as t,kg as n}from"./iframe-BULObFlw.js";import{F as r,I as i,L as a,R as o}from"./blocks-DXs5SD8z.js";var s;e((()=>{a(),i(),t(),s=(0,n(o(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
