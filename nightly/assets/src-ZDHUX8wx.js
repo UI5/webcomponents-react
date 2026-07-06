87d636622703bd962df184039780eaab61c2c650
Lukas742 <lukas.harbarth@sap.com>
2026-07-06 12:54:29 +0000

    Deploying to gh-pages from @ UI5/webcomponents-react@cbdf944bf404b0262abf83612459c89a29a44ddd 🚀

--- nightly/assets/src-ZDHUX8wx.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t}from"./LineChart-DUaHXI2J.js";var n,r=e((()=>{n={version:`2.23.2`,major:2,minor:23,patch:2,suffix:``,isNext:!1,buildTime:1783323198},n.version})),i=e((()=>{t(),r(),n.version}));export{i as t};
